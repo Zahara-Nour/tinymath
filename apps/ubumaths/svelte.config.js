b9ee56daea955db161056b340dd3e56cdea62b3e
David Le Jolly <zahara.alnour@gmail.com>
2023-02-10 17:31:50 +0300

    add adapter-vercel on docs workspace

--- apps/ubumaths/svelte.config.js
+++ apps/ubumaths/svelte.config.js
@@ -1,16 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: [
-		vitePreprocess({
-			postcss: true
-		})
-	],
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: [
+    vitePreprocess({
+      postcss: true
+    })
+  ]
 };
 
 export default config;
