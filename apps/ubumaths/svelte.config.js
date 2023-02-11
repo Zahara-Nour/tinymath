c01ac83282eba0f0cd68393285b15e59bdaf90b3
David Le Jolly <zahara.alnour@gmail.com>
2023-02-11 21:55:53 +0300

    try mathlive

--- apps/ubumaths/svelte.config.js
+++ apps/ubumaths/svelte.config.js
@@ -1,16 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 	},
 	preprocess: [
 		vitePreprocess({
-			postcss: true
-		})
-	]
-};
+			postcss: true,
+		}),
+	],
+}
 
-export default config;
+export default config
