e301782f10dcedd03b3e5448ebc4ffc4fbca16aa
David Le Jolly <zahara.alnour@gmail.com>
2023-02-22 12:10:14 +0300

    add error page

---
+++ apps/ubumaths/src/routes/+error.svelte
@@ -0,0 +1,24 @@
+<div class="bg-white flex flex-col items-center justify-center h-full">
+	<h1 class="text-primary-500" style="font-family: 'Baloo 2', sans-serif;">
+		Page introuvable !
+	</h1>
+	<img alt="error404" src="/images/spiral-pi.png" class="max-h-96 pi-spiral" />
+	<h2 class="text-black" style="font-family: 'Baloo 2', sans-serif;">
+		As-tu regardé à la 2 987 567 241<sup>ème</sup> décimale ?
+	</h2>
+</div>
+
+<style>
+	@keyframes spin {
+		from {
+			transform: rotate(0deg);
+		}
+		to {
+			transform: rotate(360deg);
+		}
+	}
+
+	.pi-spiral {
+		animation: spin infinite 20s linear;
+	}
+</style>
