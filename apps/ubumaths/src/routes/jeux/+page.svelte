1c38879933a7ad981d3f7139250b82f534d2ee82
David Le Jolly <zahara.alnour@gmail.com>
2023-03-27 09:51:45 +0300

    game links

--- apps/ubumaths/src/routes/jeux/+page.svelte
+++ apps/ubumaths/src/routes/jeux/+page.svelte
@@ -1,1 +1,11 @@
-<a href="/jeux/trio">trio</a>
+<div
+	class="flex flex-col items-center justify-center gap-20 text-4xl h-full "
+	style="font-family: 'Baloo 2', sans-serif;"
+>
+	<div>
+		<a style="text-decoration:none" href="/jeux/trio">trio</a>
+	</div>
+	<div>
+		<a style="text-decoration:none" href="/jeux/sverdle">mathémo</a>
+	</div>
+</div>
