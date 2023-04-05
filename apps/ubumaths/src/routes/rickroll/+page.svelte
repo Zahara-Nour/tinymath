bf959a9d1c94d4d678fc94714eb8df6975eaf611
David Le Jolly <zahara.alnour@gmail.com>
2023-04-05 11:22:25 +0300

    rickroll

--- apps/ubumaths/src/routes/rickroll/+page.svelte
+++ apps/ubumaths/src/routes/rickroll/+page.svelte
@@ -3,7 +3,7 @@
 	<iframe
 		width="560"
 		height="315"
-		src="https://www.youtube.com/embed/dQw4w9WgXcQ?start=43&autoplay=1"
+		src="https://www.youtube.com/embed/dQw4w9WgXcQ?start=43&end=60&controls=0&showinfo=0&loop=1&autoplay=1&fs=0&playlist=dQw4w9WgXcQ&rel=0"
 		title="YouTube video player"
 		frameborder="0"
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
