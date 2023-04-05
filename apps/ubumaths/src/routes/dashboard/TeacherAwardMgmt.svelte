1c937ef088633e27e6d52a46c4f34478d7b178a9
David Le Jolly <zahara.alnour@gmail.com>
2023-04-05 16:16:07 +0300

    improve award managemtn

--- apps/ubumaths/src/routes/dashboard/TeacherAwardMgmt.svelte
+++ apps/ubumaths/src/routes/dashboard/TeacherAwardMgmt.svelte
@@ -25,7 +25,7 @@
 	let pendingGidouille: Record<number, boolean> = {}
 	let { warn, trace, fail } = getLogger('TeacherAwardMgmt', 'warn')
 	let u = $user as Teacher
-	let selectedStudent: StudentProfile
+	let selectedStudent: StudentProfile | null = null
 	let pendingVips: boolean[] = []
 	let commons: Record<string, number> = {}
 	let uncommons: Record<string, number> = {}
@@ -66,22 +66,9 @@
 	}
 
 	async function drawVipCard() {
-		pendingVip = true
-		const { error } = await updateGidouille(
-			db,
-			u.id,
-			selectedStudent.gidouilles - 3,
-		)
-		if (error) {
-			console.log(error.message)
-			toastStore.trigger({
-				message: 'Le retrait de gidouilles a échoué.',
-				background: 'bg-error-500',
-			})
-		} else {
-			selectedStudent.gidouilles -= 3
-			// force update
-			// u = u
+		if (selectedStudent) {
+			pendingVip = true
+
 			const alea = Math.random()
 
 			const rarity =
@@ -94,7 +81,6 @@
 					: 'legendary'
 			const cards = vipCards.filter((c) => c.rarity === rarity)
 			const card = cards[Math.floor(Math.random() * cards.length)]
-			console.log('card', card.name)
 
 			const { error } = await updateVip(
 				db,
@@ -134,33 +120,36 @@
 				}
 				modalStore.trigger(d)
 			}
+
+			pendingVip = false
 		}
-		pendingVip = false
 	}
 	async function useVip(cardName: string) {
-		console.log('useVip', selectedStudent, cardName)
-		pendingVips[selectedStudent.id] = true
-		const { error } = await updateVip(db, selectedStudent.id, {
-			...selectedStudent.vips,
-			[cardName]: selectedStudent.vips[cardName] - 1,
-		})
-		if (error) {
-			console.log(error.message)
-			toastStore.trigger({
-				message: "La carte VIP n'a pas pu être utilisée.",
-				background: 'bg-error-500',
+		if (selectedStudent) {
+			console.log('useVip', selectedStudent, cardName)
+			pendingVips[selectedStudent.id] = true
+			const { error } = await updateVip(db, selectedStudent.id, {
+				...selectedStudent.vips,
+				[cardName]: selectedStudent.vips[cardName] - 1,
 			})
-		} else {
-			selectedStudent.vips[cardName] -= 1
-			console.log('card used', selectedStudent.vips[cardName])
-			toastStore.trigger({
-				message: 'La carte a été utilisée.',
-				background: 'bg-success-500',
-			})
-			// force update
-			selectedStudent = selectedStudent
+			if (error) {
+				console.log(error.message)
+				toastStore.trigger({
+					message: "La carte VIP n'a pas pu être utilisée.",
+					background: 'bg-error-500',
+				})
+			} else {
+				selectedStudent.vips[cardName] -= 1
+				console.log('card used', selectedStudent.vips[cardName])
+				toastStore.trigger({
+					message: 'La carte a été utilisée.',
+					background: 'bg-success-500',
+				})
+				// force update
+				selectedStudent = selectedStudent
+			}
+			pendingVips[selectedStudent.id] = false
 		}
-		pendingVips[selectedStudent.id] = false
 	}
 
 	async function addGidouille(student: StudentProfile) {
@@ -208,7 +197,7 @@
 	}
 </script>
 
-<PageHeader title="Récompenses / Avertissements" />
+<PageHeader title="Récompenses" />
 
 <div class="mt-8 card">
 	<header class="card-header"><h3 class="card-title">Gidouilles</h3></header>
@@ -219,7 +208,7 @@
 					<svelte:fragment slot="summary">{classe.name}</svelte:fragment>
 					<svelte:fragment slot="content">
 						<ul class="list">
-							{#each u.students[classe.id] as student (student.id)}
+							{#each u.students[classe.id].sort( (a, b) => a.firstname.localeCompare(b.firstname), ) as student (student.id)}
 								<li>
 									<div
 										class="flex  justify-between items-center w-full max-w-full text-black bg-surface-500 shadow-md rounded-md p-2 mb-2"
@@ -253,13 +242,185 @@
 											<button
 												class="btn-icon variant-filled-primary"
 												disabled={pendingGidouille[student.id]}
-												on:click={() => (selectedStudent = student)}
+												on:click={() =>
+													(selectedStudent =
+														selectedStudent === student ? null : student)}
 											>
 												<IconCards />
 											</button>
 										</div>
 									</div>
 								</li>
+								{#if selectedStudent === student}
+									<div class="mt-8 card">
+										<header
+											class="card-header flex gap-2 items-center bg-surface-500 text-black "
+										>
+											<h3 class="card-title">
+												{`cartes VIP de ${selectedStudent.firstname}`}
+											</h3>
+											<button
+												class="btn-icon variant-filled-primary"
+												disabled={pendingVip}
+												on:click={drawVipCard}
+											>
+												<IconCards />
+											</button>
+										</header>
+										<section
+											class="p-4 flex flex-col w-full max-w-full bg-surface-500 text-black shadow-md"
+										>
+											{#if Object.keys(commons).length}
+												<div class="mt-6">
+													<div class="font-bold text-2xl mb-4">Commons</div>
+													<div
+														class="flex gap-8 flex-wrap justify-center items-center"
+													>
+														{#each Object.entries(commons) as [name, count] (name)}
+															{#if count > 0}
+																<div class="flex flex-col items-center">
+																	<div class="relative inline-block">
+																		{#if count > 1}
+																			<span
+																				class="badge-icon variant-filled-success absolute -top-0 -right-0 z-10 w-10 h-10"
+																				><span class="text-white text-xl"
+																					>{count}</span
+																				></span
+																			>
+																			<VipCard {name} />
+																		{:else}
+																			<VipCard {name} />
+																		{/if}
+																	</div>
+																	<div>
+																		<button
+																			class="btn mt-2 variant-filled-primary"
+																			disabled={pendingVips[selectedStudent.id]}
+																			on:click={() => useVip(name)}
+																		>
+																			Use
+																		</button>
+																	</div>
+																</div>
+															{/if}
+														{/each}
+													</div>
+												</div>
+											{/if}
+											{#if Object.keys(uncommons).length}
+												<div class="mt-6">
+													<div class="font-bold text-2xl mb-4">Uncommons</div>
+													<div
+														class="flex gap-8 flex-wrap justify-center items-center"
+													>
+														{#each Object.entries(uncommons) as [name, count] (name)}
+															{#if count > 0}
+																<div class="flex flex-col items-center">
+																	<div class="relative inline-block">
+																		{#if count > 1}
+																			<span
+																				class="badge-icon variant-filled-success absolute -top-0 -right-0 z-10 w-10 h-10"
+																				><span class="text-white text-xl"
+																					>{count}</span
+																				></span
+																			>
+																			<VipCard {name} />
+																		{:else}
+																			<VipCard {name} />
+																		{/if}
+																	</div>
+																	<div>
+																		<button
+																			class="btn mt-2 variant-filled-primary"
+																			disabled={pendingVips[selectedStudent.id]}
+																			on:click={() => useVip(name)}
+																		>
+																			Use
+																		</button>
+																	</div>
+																</div>
+															{/if}
+														{/each}
+													</div>
+												</div>
+											{/if}
+											{#if Object.keys(rares).length}
+												<div class="mt-6">
+													<div class="font-bold text-2xl mb-4">Rares</div>
+													<div
+														class="flex gap-8 flex-wrap justify-center items-center"
+													>
+														{#each Object.entries(rares) as [name, count] (name)}
+															{#if count > 0}
+																<div class="flex flex-col items-center">
+																	<div class="relative inline-block">
+																		{#if count > 1}
+																			<span
+																				class="badge-icon variant-filled-success absolute -top-0 -right-0 z-10 w-10 h-10"
+																				><span class="text-white text-xl"
+																					>{count}</span
+																				></span
+																			>
+																			<VipCard {name} />
+																		{:else}
+																			<VipCard {name} />
+																		{/if}
+																	</div>
+																	<div>
+																		<button
+																			class="btn mt-2 variant-filled-primary"
+																			disabled={pendingVips[selectedStudent.id]}
+																			on:click={() => useVip(name)}
+																		>
+																			Use
+																		</button>
+																	</div>
+																</div>
+															{/if}
+														{/each}
+													</div>
+												</div>
+											{/if}
+											{#if Object.keys(legendaries).length}
+												<div class="mt-6">
+													<div class="font-bold text-2xl mb-4">Legendaries</div>
+													<div
+														class="flex gap-8 flex-wrap justify-center items-center"
+													>
+														{#each Object.entries(legendaries) as [name, count] (name)}
+															{#if count > 0}
+																<div class="flex flex-col items-center">
+																	<div class="relative inline-block">
+																		{#if count > 1}
+																			<span
+																				class="badge-icon variant-filled-success absolute -top-0 -right-0 z-10 w-10 h-10"
+																				><span class="text-white text-xl"
+																					>{count}</span
+																				></span
+																			>
+																			<VipCard {name} />
+																		{:else}
+																			<VipCard {name} />
+																		{/if}
+																	</div>
+																	<div>
+																		<button
+																			class="btn mt-2 variant-filled-primary"
+																			disabled={pendingVips[selectedStudent.id]}
+																			on:click={() => useVip(name)}
+																		>
+																			Use
+																		</button>
+																	</div>
+																</div>
+															{/if}
+														{/each}
+													</div>
+												</div>
+											{/if}
+										</section>
+									</div>
+								{/if}
 							{/each}
 						</ul>
 					</svelte:fragment>
@@ -268,154 +429,3 @@
 		</Accordion>
 	</section>
 </div>
-
-{#if selectedStudent}
-	<div class="mt-8 card">
-		<header class="card-header">
-			<h3 class="card-title">{`cartes VIP de ${selectedStudent.firstname}`}</h3>
-			<button
-				class="btn-icon variant-filled-primary"
-				disabled={pendingVip}
-				on:click={drawVipCard}
-			>
-				<IconCards />
-			</button>
-		</header>
-		<section
-			class="p-4 flex flex-col w-full max-w-full bg-surface-500 shadow-md"
-		>
-			{#if Object.keys(commons).length}
-				<div class="mt-6">
-					<div class="font-bold text-2xl mb-4">Commons</div>
-					<div class="flex gap-8 flex-wrap justify-center items-center">
-						{#each Object.entries(commons) as [name, count] (name)}
-							{#if count > 0}
-								<div class="flex flex-col items-center">
-									<div class="relative inline-block">
-										{#if count > 1}
-											<span
-												class="badge-icon variant-filled-success absolute -top-0 -right-0 z-10 w-10 h-10"
-												><span class="text-white text-xl">{count}</span></span
-											>
-											<VipCard {name} />
-										{:else}
-											<VipCard {name} />
-										{/if}
-									</div>
-									<div>
-										<button
-											class="btn mt-2 variant-filled-primary"
-											disabled={pendingVips[selectedStudent.id]}
-											on:click={() => useVip(name)}
-										>
-											Use
-										</button>
-									</div>
-								</div>
-							{/if}
-						{/each}
-					</div>
-				</div>
-			{/if}
-			{#if Object.keys(uncommons).length}
-				<div class="mt-6">
-					<div class="font-bold text-2xl mb-4">Uncommons</div>
-					<div class="flex gap-8 flex-wrap justify-center items-center">
-						{#each Object.entries(uncommons) as [name, count] (name)}
-							{#if count > 0}
-								<div class="flex flex-col items-center">
-									<div class="relative inline-block">
-										{#if count > 1}
-											<span
-												class="badge-icon variant-filled-success absolute -top-0 -right-0 z-10 w-10 h-10"
-												><span class="text-white text-xl">{count}</span></span
-											>
-											<VipCard {name} />
-										{:else}
-											<VipCard {name} />
-										{/if}
-									</div>
-									<div>
-										<button
-											class="btn mt-2 variant-filled-primary"
-											disabled={pendingVips[selectedStudent.id]}
-											on:click={() => useVip(name)}
-										>
-											Use
-										</button>
-									</div>
-								</div>
-							{/if}
-						{/each}
-					</div>
-				</div>
-			{/if}
-			{#if Object.keys(rares).length}
-				<div class="mt-6">
-					<div class="font-bold text-2xl mb-4">Rares</div>
-					<div class="flex gap-8 flex-wrap justify-center items-center">
-						{#each Object.entries(rares) as [name, count] (name)}
-							{#if count > 0}
-								<div class="flex flex-col items-center">
-									<div class="relative inline-block">
-										{#if count > 1}
-											<span
-												class="badge-icon variant-filled-success absolute -top-0 -right-0 z-10 w-10 h-10"
-												><span class="text-white text-xl">{count}</span></span
-											>
-											<VipCard {name} />
-										{:else}
-											<VipCard {name} />
-										{/if}
-									</div>
-									<div>
-										<button
-											class="btn mt-2 variant-filled-primary"
-											disabled={pendingVips[selectedStudent.id]}
-											on:click={() => useVip(name)}
-										>
-											Use
-										</button>
-									</div>
-								</div>
-							{/if}
-						{/each}
-					</div>
-				</div>
-			{/if}
-			{#if Object.keys(legendaries).length}
-				<div class="mt-6">
-					<div class="font-bold text-2xl mb-4">Legendaries</div>
-					<div class="flex gap-8 flex-wrap justify-center items-center">
-						{#each Object.entries(legendaries) as [name, count] (name)}
-							{#if count > 0}
-								<div class="flex flex-col items-center">
-									<div class="relative inline-block">
-										{#if count > 1}
-											<span
-												class="badge-icon variant-filled-success absolute -top-0 -right-0 z-10 w-10 h-10"
-												><span class="text-white text-xl">{count}</span></span
-											>
-											<VipCard {name} />
-										{:else}
-											<VipCard {name} />
-										{/if}
-									</div>
-									<div>
-										<button
-											class="btn mt-2 variant-filled-primary"
-											disabled={pendingVips[selectedStudent.id]}
-											on:click={() => useVip(name)}
-										>
-											Use
-										</button>
-									</div>
-								</div>
-							{/if}
-						{/each}
-					</div>
-				</div>
-			{/if}
-		</section>
-	</div>
-{/if}
