e456264bbe690599af584eb01141b7966a8f23de
David Le Jolly <zahara.alnour@gmail.com>
2023-05-09 08:43:27 +0300

    mathemo english

--- apps/ubumaths/src/routes/(app)/jeux/sverdle/words.server.ts
+++ apps/ubumaths/src/routes/(app)/jeux/sverdle/words.server.ts
@@ -1,208 +1,610 @@
 /** The list of possible words */
-export const words = [
-	'pythagore',
-	'thales',
-	'triangle',
-	'rectangle',
-	'cercle',
-	'carre',
-	'quadrilatere',
-	'hexagone',
-	'octogone',
-	'pentagone',
-	'losange',
-	'parallelogramme',
-	'trapeze',
-	'ellipse',
-	'parabole',
-	'hyperbole',
-	'cone',
-	'cylindre',
-	'prisme',
-	'pyramide',
-	'cube',
-	'pave',
-	'symetrie',
-	'rotation',
-	'homothetie',
-	'translation',
-	'axe',
-	'diagonale',
-	'perpendiculaire',
-	'parallele',
-	'angle',
-	'aire',
-	'volume',
-	'perimetre',
-	'centre',
-	'rayon',
-	'diametre',
-	'hauteur',
-	'base',
-	'coordonnees',
+export const words_french = [
 	'abscisse',
-	'ordonnee',
-	'origine',
-	'vecteur',
-	'produit',
-	'somme',
-	'difference',
-	'oppose',
-	'quotient',
-	'sommets',
-	'aretes',
-	'faces',
-	'equation',
 	'addition',
-	'soustraction',
-	'multiplication',
-	'division',
-	'racine',
-	'puissance',
-	'logarithme',
-	'proportionnalite',
-	'cosinus',
-	'sinus',
-	'tangente',
-	'hypothenuse',
-	'fraction',
-	'entier',
-	'rationnel',
-	'irrationnel',
-	'positif',
-	'negatif',
-	'nombre',
-	'longueur',
-	'largeur',
-	'inconnue',
-	'repere',
-	'plan',
-	'espace',
-	'factorisation',
-	'developpement',
-	'simplification',
-	'cote',
-	'suite',
-	'convergente',
-	'divergente',
-	'limite',
+	'adjacent',
+	'aigu',
+	'aire',
+	'algebre',
+	'aligne',
+	'angle',
+	'antecedent',
+	'arete',
+	'arete',
+	'arithmetique',
+	'arrondi',
+	'axe',
+	'barycentre',
+	'base',
+	'bissectrice',
+	'calcul',
+	'calculatrice',
+	'carre',
+	'centre',
+	'cercle',
+	'chiffre',
+	'codage',
+	'coefficient',
+	'combinaison',
+	'compas',
+	'complementaire',
+	'compter',
+	'concave',
+	'cone',
+	'conjecture',
+	'consecutif',
+	'continue',
 	'continuite',
+	'convergente',
+	'conversion',
+	'convexe',
+	'coordonnees',
+	'cosinus',
+	'cote',
+	'courbe',
+	'croissant',
+	'cube',
+	'cylindre',
+	'decimal',
+	'decroissant',
+	'demonstration',
+	'denominateur',
+	'denominateur',
 	'derivee',
-	'integrale',
-	'fonction',
-	'polynome',
-	'relatif',
-	'superieur',
-	'inferieur',
+	'developpement',
+	'diagonale',
+	'diametre',
+	'difference',
+	'distance',
+	'divergente',
+	'diviseur',
+	'divisible',
+	'division',
+	'double',
+	'droite',
 	'egal',
 	'egalite',
+	'ellipse',
+	'ensemble',
+	'entier',
+	'equation',
+	'equidistant',
+	'equilateral',
+	'equivalence',
+	'espace',
+	'evenement',
+	'exponentielle',
+	'extremite',
+	'face',
+	'faces',
+	'facteur',
+	'factorisation',
+	'figure',
+	'fonction',
+	'formule',
+	'fraction',
+	'geometrie',
+	'grandeur',
+	'hauteur',
+	'hexagone',
+	'homothetie',
+	'hyperbole',
+	'hypothenuse',
+	'hypothese',
+	'image',
+	'impair',
+	'implication',
+	'inconnue',
 	'inegalite',
 	'inequation',
-	'compas',
-	'regle',
-	'calculatrice',
-	'rapporteur',
-	'point',
-	'droite',
-	'segment',
-	'isocele',
-	'equilateral',
-	'equidistant',
-	'mediane',
-	'bissectrice',
-	'mediatrice',
-	'solide',
-	'extremite',
-	'primitive',
-	'intersection',
-	'probabilite',
-	'evenement',
-	'ensemble',
-	'complementaire',
-	'facteur',
-	'diviseur',
-	'multiple',
-	'premier',
-	'propriete',
-	'formule',
-	'unite',
-	'conversion',
-	'croissant',
-	'decroissant',
-	'double',
-	'moitie',
-	'quart',
-	'tiers',
-	'triple',
-	'quadruple',
-	'terme',
-	'numerateur',
-	'denominateur',
-	'theoreme',
-	'moyenne',
-	'pourcentage',
-	'algebre',
-	'arithmetique',
-	'calcul',
-	'chiffre',
-	'numerateur',
-	'denominateur',
-	'decimal',
-	'coefficient',
-	'adjacent',
-	'antecedent',
-	'image',
-	'convexe',
-	'concave',
-	'distance',
-	'intervalle',
-	'norme',
-	'milieu',
-	'barycentre',
-	'orthogonal',
-	'permutation',
-	'reciproque',
-	'aigu',
-	'obtus',
-	'combinaison',
-	'litteral',
-	'continue',
-	'divisible',
-	'exponentielle',
-	'grandeur',
-	'arrondi',
-	'geometrie',
-	'trigonometrie',
-	'sphere',
-	'hypothese',
-	'conjecture',
-	'demonstration',
-	'implication',
-	'equivalence',
+	'inferieur',
 	'infini',
-	'pair',
-	'impair',
-	'face',
-	'arete',
-	'sommet',
-	'mesure',
-	'operation',
-	'polygone',
-	'raisonnement',
-	'aligne',
-	'consecutif',
-	'solution',
-	'secant',
-	'courbe',
-	'shisma',
-	'schema',
-	'compter',
-	'codage',
-	'figure',
-	'signe',
+	'integrale',
+	'intersection',
+	'intervalle',
 	'inverse',
+	'irrationnel',
+	'isocele',
+	'largeur',
+	'limite',
+	'litteral',
+	'logarithme',
+	'longueur',
+	'losange',
+	'mediane',
+	'mediatrice',
+	'mesure',
+	'milieu',
+	'moitie',
+	'moyenne',
+	'multiple',
+	'multiplication',
+	'negatif',
+	'nombre',
+	'norme',
+	'numerateur',
+	'obtus',
+	'octogone',
+	'operation',
+	'oppose',
+	'ordonnee',
+	'origine',
+	'orthogonal',
+	'pair',
+	'parabole',
+	'parallele',
+	'parallelogramme',
+	'pave',
+	'pentagone',
+	'perimetre',
+	'permutation',
+	'perpendiculaire',
+	'plan',
+	'point',
+	'polygone',
+	'polynome',
+	'positif',
+	'pourcentage',
+	'premier',
+	'primitive',
+	'prisme',
+	'probabilite',
+	'produit',
+	'proportionnalite',
+	'propriete',
+	'puissance',
+	'pyramide',
+	'pythagore',
+	'quadrilatere',
+	'quadruple',
+	'quart',
+	'quotient',
+	'racine',
 	'radian',
+	'raisonnement',
 	'rapport',
+	'rapporteur',
+	'rationnel',
+	'rayon',
+	'reciproque',
+	'rectangle',
+	'regle',
+	'relatif',
+	'repere',
+	'rotation',
+	'schema',
+	'secant',
+	'segment',
+	'shisma',
+	'signe',
+	'simplification',
+	'sinus',
+	'solide',
+	'solution',
+	'somme',
+	'sommet',
+	'soustraction',
+	'sphere',
+	'suite',
+	'superieur',
+	'symetrie',
+	'tangente',
+	'terme',
+	'thales',
+	'theoreme',
+	'tiers',
+	'translation',
+	'trapeze',
+	'triangle',
+	'trigonometrie',
+	'triple',
+	'unite',
+	'vecteur',
+	'volume',
+]
+
+/*
+
+Here are the translations of the mathematical terms you provided:
+
+ abscissa: abscissa
+ addition: addition
+ adjacent: adjacent
+ aigu: acute
+ aire: area
+ algèbre: algebra
+ aligné: aligned
+ angle: angle
+ antécédent: antecedent
+ arête: edge
+ arête: edge
+ arithmétique: arithmetic
+ arrondi: rounded
+ axe: axis
+ barycentre: barycenter
+ base: base
+ bissectrice: bisector
+ calcul: calculation
+ calculatrice: calculator
+ carré: square
+ centre: center
+ cercle: circle
+ chiffre: digit
+ codage: encoding
+ coefficient: coefficient
+ combinaison: combination
+ compas: compass
+ complémentaire: complementary
+ compter: count
+ concave: concave
+ cône: cone
+ conjecture: conjecture
+ consécutif: consecutive
+ continue: continuous
+ continuité: continuity
+ convergente: convergent
+ conversion: conversion
+ convexe: convex
+ coordonnées: coordinates
+ cosinus: cosine
+ côté: side
+ courbe: curve
+ croissant: increasing
+ cube: cube
+ cylindre: cylinder
+ décimal: decimal
+ décroissant: decreasing
+ démonstration: proof
+ dénominateur: denominator
+ dénominateur: denominator
+ dérivée: derivative
+ développement: expansion
+ diagonale: diagonal
+ diamètre: diameter
+ différence: difference
+ distance: distance
+ divergente: divergent
+ diviseur: divisor
+ divisible: divisible
+ division: division
+ double: double
+ droite: line
+ égal: equal
+ égalité: equality
+ ellipse: ellipse
+ ensemble: set
+ entier: integer
+ équation: equation
+ equidistant: equidistant
+ équilatéral: equilateral
+ équivalence: equivalence
+ espace: space
+ événement: event
+ exponentielle: exponential
+ extrémité: endpoint
+ face: face
+ faces: faces
+ facteur: factor
+ factorisation: factorization
+ figure: figure
+ fonction: function
+ formule: formula
+ fraction: fraction
+ géométrie: geometry
+ grandeur: magnitude
+ hauteur: height
+ hexagone: hexagon
+ homothétie: homothety
+ hyperbole: hyperbola
+ hypothénuse: hypotenuse
+ hypothèse: hypothesis
+ image: image
+ impair: odd
+ implication: implication
+ inconnue: unknown
+ inégalité: inequality
+ inéquation: inequation
+ inférieur: less than
+ infini: infinite
+ intégrale: integral
+ intersection: intersection
+ intervalle: interval
+ inverse: inverse
+ irrationnel: irrational
+ isocèle: isosceles
+ largeur: width
+ limite: limit
+ littéral: literal
+ logarithme: logarithm
+ longueur: length
+ losange: rhombus
+ médiane: median
+ médiatrice: perpendicular bisector
+ mesure: measurement
+ milieu: midpoint
+ moitié: half
+ moyenne: average
+Here are the translations of the mathematical terms you provided:
+
+ 'average' (moyenne)
+ 'multiple' (multiple)
+ 'multiplication' (multiplication)
+ 'negative' (négatif)
+ 'number' (nombre)
+ 'norm' (norme)
+ 'numerator' (numérateur)
+ 'obtuse' (obtus)
+ 'octagon' (octogone)
+ 'operation' (opération)
+ 'opposite' (opposé)
+ 'ordinate' (ordonnée)
+ 'origin' (origine)
+ 'orthogonal' (orthogonal)
+ 'even' (pair)
+ 'parabola' (parabole)
+ 'parallel' (parallèle)
+ 'parallelogram' (parallélogramme)
+ 'cube' (pavé)
+ 'pentagon' (pentagone)
+ 'perimeter' (périmètre)
+ 'permutation' (permutation)
+ 'perpendicular' (perpendiculaire)
+ 'plane' (plan)
+ 'point' (point)
+ 'polygon' (polygone)
+ 'polynomial' (polynôme)
+ 'positive' (positif)
+ 'percentage' (pourcentage)
+ 'prime' (premier)
+ 'primitive' (primitive)
+ 'prism' (prisme)
+ 'probability' (probabilité)
+ 'product' (produit)
+ 'proportionality' (proportionnalité)
+ 'property' (propriété)
+ 'power' (puissance)
+ 'pyramid' (pyramide)
+ 'Pythagoras' (Pythagore)
+ 'quadrilateral' (quadrilatère)
+ 'quadruple' (quadruple)
+ 'quarter' (quart)
+ 'quotient' (quotient)
+ 'root' (racine)
+ 'radian' (radian)
+ 'reasoning' (raisonnement)
+ 'ratio' (rapport)
+ 'protractor' (rapporteur)
+ 'rational' (rationnel)
+ 'radius' (rayon)
+ 'reciprocal' (réciproque)
+ 'rectangle' (rectangle)
+ 'rule' (règle)
+ 'relative' (relatif)
+ 'coordinate system' (repère)
+ 'rotation' (rotation)
+ 'diagram' (schéma)
+ 'secant' (sécante)
+ 'segment' (segment)
+ 'chord' (chord)
+ 'sign' (signe)
+ 'simplification' (simplification)
+ 'sine' (sinus)
+ 'solid' (solide)
+ 'solution' (solution)
+ 'sum' (somme)
+ 'vertex' (sommet)
+ 'subtraction' (soustraction)
+ 'sphere' (sphère)
+ 'sequence' (suite)
+ 'greater than' (supérieur)
+ 'symmetry' (symétrie)
+ 'tangent' (tangente)
+ 'term' (terme)
+ 'Thales' (Thalès)
+ 'theorem' (théorème)
+ 'third' (tiers)
+ 'translation' (translation)
+ 'trapezoid' (trapèze)
+ 'triangle' (triangle)
+ 'trigonometry' (trigonométrie)
+ 'triple' (triple)
+ 'unit' (unité)
+ 'vector' (vect
+*/
+export const words = [
+	'abscissa',
+	'addition',
+	'adjacent',
+	'acute',
+	'area',
+	'algebra',
+	'aligned',
+	'angle',
+	'antecedent',
+	'edge',
+	'arithmetic',
+	'rounded',
+	'axis',
+	'barycenter',
+	'base',
+	'bisector',
+	'calculation',
+	'calculator',
+	'square',
+	'center',
+	'circle',
+	'digit',
+	'encoding',
+	'coefficient',
+	'combination',
+	'compass',
+	'complementary',
+	'count',
+	'concave',
+	'cone',
+	'conjecture',
+	'consecutive',
+	'continuous',
+	'continuity',
+	'convergent',
+	'conversion',
+	'convex',
+	'coordinates',
+	'cosine',
+	'side',
+	'curve',
+	'increasing',
+	'cube',
+	'cylinder',
+	'decimal',
+	'decreasing',
+	'proof',
+	'denominator',
+	'derivative',
+	'expansion',
+	'diagonal',
+	'diameter',
+	'difference',
+	'distance',
+	'divergent',
+	'divisor',
+	'divisible',
+	'division',
+	'double',
+	'line',
+	'equal',
+	'equality',
+	'ellipse',
+	'set',
+	'integer',
+	'equation',
+	'equidistant',
+	'equilateral',
+	'equivalence',
+	'space',
+	'event',
+	'exponential',
+	'endpoint',
+	'face',
+	'factor',
+	'factorization',
+	'figure',
+	'function',
+	'formula',
+	'fraction',
+	'geometry',
+	'magnitude',
+	'height',
+	'hexagon',
+	'homothety',
+	'hyperbola',
+	'hypotenuse',
+	'hypothesis',
+	'image',
+	'odd',
+	'implication',
+	'unknown',
+	'inequality',
+	'inequation',
+	'infinite',
+	'integral',
+	'intersection',
+	'interval',
+	'inverse',
+	'irrational',
+	'isosceles',
+	'width',
+	'limit',
+	'literal',
+	'logarithm',
+	'length',
+	'rhombus',
+	'median',
+	'measurement',
+	'midpoint',
+	'half',
+	'average',
+	'multiple',
+	'multiplication',
+	'negative',
+	'number',
+	'norm',
+	'numerator',
+	'obtuse',
+	'octagon',
+	'operation',
+	'opposite',
+	'ordinate',
+	'origin',
+	'orthogonal',
+	'even',
+	'parabola',
+	'parallel',
+	'parallelogram',
+	'cube',
+	'pentagon',
+	'perimeter',
+	'permutation',
+	'perpendicular',
+	'plane',
+	'point',
+	'polygon',
+	'polynomial',
+	'positive',
+	'percentage',
+	'prime',
+	'primitive',
+	'prism',
+	'probability',
+	'product',
+	'proportionality',
+	'property',
+	'power',
+	'pyramid',
+	'Pythagoras',
+	'quadrilateral',
+	'quadruple',
+	'quarter',
+	'quotient',
+	'root',
+	'radian',
+	'reasoning',
+	'ratio',
+	'protractor',
+	'rational',
+	'radius',
+	'reciprocal',
+	'rectangle',
+	'rule',
+	'relative',
+	'rotation',
+	'diagram',
+	'secant',
+	'segment',
+	'chord',
+	'sign',
+	'simplification',
+	'sine',
+	'solid',
+	'solution',
+	'sum',
+	'vertex',
+	'subtraction',
+	'sphere',
+	'sequence',
+	'symmetry',
+	'tangent',
+	'term',
+	'Thales',
+	'theorem',
+	'third',
+	'translation',
+	'trapezoid',
+	'triangle',
+	'trigonometry',
+	'triple',
+	'unit',
+	'vector',
 ]
 
 /** The list of valid guesses, of which the list of possible words is a subset */
