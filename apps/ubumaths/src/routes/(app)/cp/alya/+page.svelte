704d21cc8b48faafada5dfa40a2abcf7ecb29d80
David Le Jolly <zahara.alnour@gmail.com>
2023-11-05 13:45:19 +0300

    update cp

---
+++ apps/ubumaths/src/routes/(app)/cp/alya/+page.svelte
@@ -0,0 +1,19 @@
+<div class="text-center"><h1>Alya</h1></div>
+
+<div class="p-2">
+	<h3 class="my-2">Calcul mental</h3>
+	<div class="my-4">
+		<a
+			target="_blank"
+			href="https://ubumaths.net/automaths/assessment/?questions=%5B%7B%22id%22:%220310%22,%22count%22:10,%22delay%22:20%7D%5D"
+			>Multiplications</a
+		>
+	</div>
+	<div class="my-4">
+		<a
+			target="_blank"
+			href="https://ubumaths.net/automaths/assessment/?questions=%5B%7B%22id%22:%220400%22,%22count%22:10,%22delay%22:20%7D%5D"
+			>Divisions</a
+		>
+	</div>
+</div>
