644ce063198853938613ceddc7045c3b6166c887
David Le Jolly <zahara.alnour@gmail.com>
2023-11-06 21:24:28 +0300

    cp alya

--- apps/ubumaths/src/routes/(app)/cp/alya/+page.svelte
+++ apps/ubumaths/src/routes/(app)/cp/alya/+page.svelte
@@ -16,4 +16,34 @@
 			>Divisions</a
 		>
 	</div>
+
+	<h2 class="my-6">Fractions</h2>
+	<div class="my-4">
+		<a
+			target="_blank"
+			href="https://ubumaths.net/automaths/assessment/?questions=%5B%7B%22id%22:%223030%22,%22count%22:10,%22delay%22:12000%7D%5D"
+			>Egalité de fractions 1</a
+		>
+	</div>
+	<div class="my-4">
+		<a
+			target="_blank"
+			href="https://ubumaths.net/automaths/assessment/?questions=%5B%7B%22id%22:%223031%22,%22count%22:10,%22delay%22:30%7D%5D"
+			>Egalité de fractions 2</a
+		>
+	</div>
+	<div class="my-4">
+		<a
+			target="_blank"
+			href="https://ubumaths.net/automaths/assessment/?questions=%5B%7B%22id%22:%223040%22,%22count%22:10,%22delay%22:20%7D%5D"
+			>Simplification de fractions 1</a
+		>
+	</div>
+	<div class="my-4">
+		<a
+			target="_blank"
+			href="https://ubumaths.net/automaths/assessment/?questions=%5B%7B%22id%22:%223041%22,%22count%22:10,%22delay%22:20%7D%5D"
+			>Simplification de fractions 2</a
+		>
+	</div>
 </div>
